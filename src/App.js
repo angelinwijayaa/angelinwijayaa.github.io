ef8dfd2e57b7bfef72c152648228c97bbfdfe0f1
Grant Tannert <grtmega10@gmail.com>
2023-06-15 13:53:07 -0700

    added sliding background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './button.css';
 
 function Topnav(props) {
   return (
@@ -8,9 +9,14 @@ function Topnav(props) {
           <p>💗 Welcome to Purefoams Shop! 💗</p>
       </div>
       <div className="grid-cols-1">
+        {/* <div className="btn-one">Home</div>
+        <div className="btn-one">Shop all</div>
+        <div className="btn-one">New arrival</div>
+        <div className="btn-one">Sales</div>
+        <div className="btn-one">FAQs</div> */}
         <button>Home</button>
         <button>Shop all</button>
-        <button>New arrival</button>
+        <button>New arrivals</button>
         <button>Sales</button>
         <button>FAQs</button>
       </div>
@@ -22,6 +28,9 @@ function App() {
   return (
     <div>
       <Topnav />
+      <div className="wrapper">
+        <div className="sliding-background"></div>
+      </div>
     </div>
   );
 }
