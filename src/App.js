0b5b484f38165b5d90df63829d58da90ebc98ccc
Grant Tannert <grtmega10@gmail.com>
2023-06-16 03:42:47 -0700

    adjusted all positions of elements from static to relative or abs added new gif background to nav made navigation fixed added new text shadow for title and nav options added z index for some elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import './button.css';
+import './index.css'
+import snacks from "./img/snacks.png"
+import petal from "./img/petals.gif"
 
 function Topnav(props) {
   return (
@@ -24,13 +27,32 @@ function Topnav(props) {
   );
 }
 
+function Slider(props) {
+  return (
+    <div className="wrapper">
+      <div className="sliding-background"></div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <Topnav />
-      <div className="wrapper">
-        <div className="sliding-background"></div>
+      <Slider />
+      <div className="petal-wrapper">
+        <img src={petal} className="petal" />
       </div>
+      {/* <div className="grid-cols-2">
+        <div className="cols-2-1">
+          <div className="about-title">About Purefoams Shop</div>
+          <div className="about-desc">
+            <img src={snacks} width="200rem"/>
+          </div>
+        </div>
+        <div className="cols-2-2"></div>
+        <div clasName="cols-2-3"></div>
+      </div> */}
     </div>
   );
 }
